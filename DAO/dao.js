58c23e2a6da7afbecb9892b1dd5fe341c69ee996
01110101 <42702073+alisherweb3@users.noreply.github.com>
2022-12-06 03:32:30 +0500

    Create dao.js

---
+++ DAO/dao.js
@@ -0,0 +1,11 @@
+- history and varieties DAO
+
+- Governor Bravo vs MolochDAO
+
+- Multisig-wallet Gnosis Safe
+
+- Tokengate-services for Discord
+
+- Offchain-voting on snapshot
+
+- Tracking contributers and compensations
